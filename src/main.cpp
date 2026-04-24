9bbe43f959dd4242f5b6ab057bad4c4d5dfd93e1
EMILIANO <a24110258@live.ceti.mx>
2026-04-24 01:06:16 +0000

    Práctica 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <Foco.hpp>
 
 int main(int argc, char const *argv[])
 {
+    Foco foquito;
+
+    foquito.Encender();
+    foquito.Apagar();
+
+    std::cout 
+    << "Estado Foco: " 
+    << foquito.LeerEstado()
+    << std::endl;
 
     return 0;
 }
